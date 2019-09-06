689da0f73a14b83a97925c9954031a42234ae399
Rituvesh Kumar <rituvesh@gmail.com>
2019-09-06 10:13:31 +0200

    WIP

--- pdl-fullmakt-app/src/main/java/no/nav/pdl/fullmakt/app/common/CommonConfig.java
+++ pdl-fullmakt-app/src/main/java/no/nav/pdl/fullmakt/app/common/CommonConfig.java
@@ -1,14 +1,10 @@
 package no.nav.pdl.fullmakt.app.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.hotspot.DefaultExports;
-import no.nav.pdl.fullmakt.app.common.utils.JsonUtils;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,23 +12,12 @@ import org.springframework.web.client.RestTemplate;
 @EnableTransactionManagement
 public class CommonConfig {
 
-	@Primary
-	@Bean
-	public ObjectMapper objectMapper() {
-		return JsonUtils.getObjectMapper();
-	}
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
 
-	@Bean
-	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-		jsonConverter.setObjectMapper(objectMapper());
-		return jsonConverter;
-	}
 
 	/**
 	 * Make sure spring uses the defaultRegistry
--- pdl-fullmakt-app/src/main/java/no/nav/pdl/fullmakt/app/common/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package no.nav.pdl.fullmakt.app.common.utils;
-
-public final class Constants {
-
-	private Constants() {
-		throw new IllegalStateException("Utility class");
-	}
-
-}
